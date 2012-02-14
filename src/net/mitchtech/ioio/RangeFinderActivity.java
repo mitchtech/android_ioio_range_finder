957c52eb3dc709aa963af520f77bcea6d2116dfa
Michael Mitchell <michael@mitchtech.net>
2012-02-13 19:03:08 -0500

    initial commit

---
+++ src/net/mitchtech/ioio/RangeFinderActivity.java
@@ -0,0 +1,89 @@
+package net.mitchtech.ioio;
+
+import ioio.lib.api.AnalogInput;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.AbstractIOIOActivity;
+import net.mitchtech.ioio.rangefinder.R;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class RangeFinderActivity extends AbstractIOIOActivity {
+	private static final int INFRARED_PIN = 34;
+	private static final int ULTRASONIC_PIN = 35;
+
+	TextView mXTextView, mYTextView;
+
+	SeekBar mXSeekBar, mYSeekBar;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
+
+		mXTextView = (TextView) findViewById(R.id.tvX);
+		mYTextView = (TextView) findViewById(R.id.tvY);
+
+		mXSeekBar = (SeekBar) findViewById(R.id.sbX);
+		mYSeekBar = (SeekBar) findViewById(R.id.sbY);
+
+	}
+
+	class IOIOThread extends AbstractIOIOActivity.IOIOThread {
+		private AnalogInput mXInput, mYInput;
+
+		@Override
+		public void setup() throws ConnectionLostException {
+			try {
+				mXInput = ioio_.openAnalogInput(INFRARED_PIN);
+				mYInput = ioio_.openAnalogInput(ULTRASONIC_PIN);
+			} catch (ConnectionLostException e) {
+				throw e;
+			}
+		}
+
+		@Override
+		public void loop() throws ConnectionLostException {
+			try {
+				final float xValue = mXInput.read();
+				final float yValue = mYInput.read();
+
+				setSeekBars((int) (xValue * 100), ((int) (yValue * 100)));
+
+				setText(Float.toString((xValue * 100)), Float.toString((yValue * 100)));
+				sleep(10);
+			} catch (InterruptedException e) {
+				ioio_.disconnect();
+			} catch (ConnectionLostException e) {
+				throw e;
+			}
+		}
+	}
+
+	@Override
+	protected AbstractIOIOActivity.IOIOThread createIOIOThread() {
+		return new IOIOThread();
+	}
+
+	private void setSeekBars(final int xValue, final int yValue) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				mXSeekBar.setProgress(xValue);
+				mYSeekBar.setProgress(yValue);
+			}
+		});
+	}
+
+	private void setText(final String xText, final String yText) {
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				mXTextView.setText(xText);
+				mYTextView.setText(yText);
+			}
+		});
+	}
+}
